4e3e27c7e99da5ceeb9ae977588d021a77f614eb
Otávio Pinheiro <otavioaecpinheiro@gmail.com>
2022-12-30 21:42:03 -0300

    feat: Desenvolvendo a movimentação dos fantasmas.

--- fogefoge.rb
+++ fogefoge.rb
@@ -32,6 +32,25 @@ def calcula_nova_posicao(heroi, direcao)
   heroi
 end
 
+def move_fantasma(mapa, linha, coluna)
+  mapa[linha][coluna] = " "
+  linha += 0
+  coluna += 1
+  mapa[linha][coluna] = "F"
+end
+
+def move_fantasmas(mapa)
+  caractere_do_fantasma = "F"
+  mapa.each_with_index do |linha_atual, linha|
+    linha_atual.chars.each_with_index do |caractere_atual, coluna|
+      eh_fantasma = caractere_atual == caractere_do_fantasma
+      if eh_fantasma
+        move_fantasma(mapa, linha, coluna)
+      end
+    end
+  end
+end
+
 def posicao_valida?(mapa, nova_posicao)
   linhas = mapa.size
   colunas = mapa[0].size
@@ -46,7 +65,7 @@ def posicao_valida?(mapa, nova_posicao)
 end
 
 def joga(nome)
-  mapa = le_mapa(1)
+  mapa = le_mapa(2)
   while true
     desenha(mapa)
     heroi = encontra_jogador(mapa)
@@ -62,6 +81,8 @@ def joga(nome)
     puts "#{nova_posicao}"
     mapa[heroi[0]][heroi[1]] = " "
     mapa[nova_posicao[0]][nova_posicao[1]] = "H"
+    
+    move_fantasmas(mapa)
   end
 end
 
